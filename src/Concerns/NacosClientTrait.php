4aff7ce1b8330ca4172ef4df791d1ef52cdd5424
jundayw <jundayw@126.com>
2024-08-24 12:20:41 +0800

    refactor:NacosClientTrait

---
+++ src/Concerns/NacosClientTrait.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Nacosvel\Nacos\Concerns;
+
+use GuzzleHttp\ClientInterface;
+use Nacosvel\Nacos\Contracts\NacosRequestInterface;
+use Nacosvel\Nacos\Contracts\NacosResponseInterface;
+use Nacosvel\OpenHttp\Builder;
+
+trait NacosClientTrait
+{
+    /**
+     * @return NacosRequestInterface
+     */
+    public function getRequest(): NacosRequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param NacosRequestInterface $request
+     *
+     * @return static
+     */
+    public function setRequest(NacosRequestInterface $request): static
+    {
+        $this->request = $request;
+        return $this;
+    }
+
+    /**
+     * @return NacosResponseInterface
+     */
+    public function getResponse(): NacosResponseInterface
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param NacosResponseInterface $response
+     *
+     * @return static
+     */
+    public function setResponse(NacosResponseInterface $response): static
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+
+    /**
+     * @return ClientInterface
+     */
+    public function getClient(): ClientInterface
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param ClientInterface $client
+     *
+     * @return static
+     */
+    public function setClient(ClientInterface $client): static
+    {
+        $clientDecorator = Builder::factory()->getClient()->setRequestClient($client);
+        $client          = $clientDecorator->getRequestClient();
+        $handler         = $clientDecorator->getConfig('handler');
+        $handler->remove('nacosvel.nacos_sdk_php.nacos_auth_middleware');
+        $handler->push($this->getRequest()->getAuth()->middleware($this), 'nacosvel.nacos_sdk_php.nacos_auth_middleware');
+        $this->client = $client;
+        return $this;
+    }
+
+}
