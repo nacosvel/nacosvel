3179e72584b1477f882f761079293515aa31f90d
ccboxers <892349689@qq.com>
2024-08-29 14:08:25 +0800

    feat:NamespaceRequest

--- src/Console/NamespaceRequest.php
+++ src/Console/NamespaceRequest.php
@@ -7,5 +7,43 @@ use Nacosvel\NacosClient\NacosRequestResponse;
 
 class NamespaceRequest extends NacosRequestResponse implements NamespaceInterface
 {
+    /**
+     * Request Method
+     *
+     * @var string
+     */
+    protected string $method = self::GET;
+
+    /**
+     * Request Uri
+     *
+     * @var array|string
+     */
+    protected array|string $uri = '/nacos/v2/console/namespace';
+
+    public function v2(string $namespaceId): NamespaceInterface
+    {
+        return $this->setNamespaceId($namespaceId);
+    }
+
+    protected string $namespaceId;
+
+    /**
+     * @return string
+     */
+    public function getNamespaceId(): string
+    {
+        return $this->namespaceId;
+    }
+
+    /**
+     * @param string $namespaceId
+     *
+     * @return static
+     */
+    public function setNamespaceId(string $namespaceId): static
+    {
+        return $this->parameter('namespaceId', $this->namespaceId = $namespaceId);
+    }
 
 }
--- src/Contracts/Console/NamespaceInterface.php
+++ src/Contracts/Console/NamespaceInterface.php
@@ -6,5 +6,16 @@ use Nacosvel\NacosClient\Contracts\V2\VersionInterface;
 
 interface NamespaceInterface extends VersionInterface
 {
+    /**
+     * @return string
+     */
+    public function getNamespaceId(): string;
+
+    /**
+     * @param string $namespaceId
+     *
+     * @return static
+     */
+    public function setNamespaceId(string $namespaceId): static;
 
 }
