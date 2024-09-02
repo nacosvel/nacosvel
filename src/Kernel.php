b7f8f497ebfb527aa90e4f21744bd8e27ac9fbac
jundayw <jundayw@126.com>
2024-09-02 17:43:21 +0800

    refactor:v1.0.0

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,7 +3,10 @@
 namespace Nacosvel\Console;
 
 use Exception;
+use Nacosvel\Console\Command\Command;
 use Nacosvel\Console\Exception\NacosvelConsoleException;
+use Nacosvel\Package\Contracts\PackageManifestInterface;
+use Nacosvel\Package\PackageManifest;
 use Override;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,17 +14,57 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Kernel extends Application
 {
-    protected static array $bootstraps = [];
+    protected static array             $bootstraps = [];
+    protected PackageManifestInterface $packageManifest;
 
-    public function __construct(string $name = 'Nacosvel Console', string $version = '1.0.0')
+    public function __construct(protected string $cacheCommandFile = 'nacosvel/command.json')
     {
-        parent::__construct($name, $version);
+        parent::__construct('Nacosvel Console', '1.0.0');
+        $this->packageManifest = new PackageManifest();
+        $this->setCacheCommandFile($this->cacheCommandFile);
     }
 
-    public function getBootstraps(): array
+    /**
+     * @return string
+     */
+    public function getCacheCommandFile(): string
     {
-        if (count(self::$bootstraps) == 0) {
-            self::$bootstraps = [];
+        return $this->cacheCommandFile;
+    }
+
+    /**
+     * @param string $cacheCommandFile
+     *
+     * @return Kernel
+     */
+    public function setCacheCommandFile(string $cacheCommandFile): Kernel
+    {
+        $path                   = $this->packageManifest->getVendorPath('composer');
+        $this->cacheCommandFile = $this->packageManifest->combinePaths($path, $cacheCommandFile);
+        return $this;
+    }
+
+    protected function getCacheCommands(): array
+    {
+        if (!is_file($this->getCacheCommandFile())) {
+            return [];
+        }
+
+        $cache = json_decode(file_get_contents($this->getCacheCommandFile()), true) ?? [];
+
+        return array_unique(array_merge(...array_values($cache)));
+    }
+
+    protected function getBootstraps(): array
+    {
+        if (self::$bootstraps) {
+            return self::$bootstraps;
+        }
+
+        foreach ($this->getCacheCommands() as $command) {
+            if (class_exists($command) && is_subclass_of($command, Command::class)) {
+                self::$bootstraps[] = new $command();
+            }
         }
 
         return self::$bootstraps;
