a840a7a8981e52d3938fa0d54642aba848095307
jundayw <jundayw@126.com>
2024-09-10 00:41:45 +0800

    wip

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -10,6 +10,32 @@ abstract class Configuration implements ConfigurationInterface
 {
     protected string $defaultMethod = RequestMethod::POST;
 
+    protected array $consumerMap = [
+        RequestMethod::GET     => 'query',
+        RequestMethod::POST    => 'form_params',// multipart, json, form_params
+        RequestMethod::PUT     => 'form_params',// json, body, form_params
+        RequestMethod::DELETE  => 'query',// json, body, query
+        RequestMethod::PATCH   => 'form_params',// json, body, form_params
+        RequestMethod::OPTIONS => 'query',
+        RequestMethod::HEAD    => 'query',
+    ];
+
+    protected array $producerMap = [
+        'data',
+        // 'list',// data.list
+    ];
+
+    /**
+     * Attempt to convert $key to $value based on return type
+     *
+     * @var array<mixed, mixed|(callable(mixed,int): mixed)>
+     */
+    protected array $transformationMap = [
+        // User::class => Model::class,
+        // Collection::class => Collection::class,
+        // ArrayObject::class => function ($value, $key) {},
+    ];
+
     final public function register(FeignInterface $factory): void
     {
         $factory->bind(ConfigurationInterface::class, function () {
