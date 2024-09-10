52670d291be7492f5947522ec894d7a8bb94ea03
jundayw <jundayw@126.com>
2024-09-10 14:50:29 +0800

    chore:DiscoverInterface

--- src/Contracts/DiscoverInterface.php
+++ src/Contracts/DiscoverInterface.php
@@ -2,7 +2,15 @@
 
 namespace Nacosvel\Interop\Container\Contracts;
 
+use Psr\Container\ContainerInterface;
+
 interface DiscoverInterface
 {
+    public static function container(
+        ?ContainerInterface $container = null,
+        callable|string     $bind = 'bind',
+        callable|string     $make = 'make',
+        callable|string     $resolving = 'resolving'
+    ): NacosvelInterface;
 
 }
